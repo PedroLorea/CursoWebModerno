c177e80819b6e110e8899a1eb5c51294cd4e799b
PedroLorea <p.lorea@edu.pucrs.br>
2023-04-04 16:10:14 -0300

    commit

---
+++ react/exercicios/src/componentes/Primeiro.jsx
@@ -0,0 +1,14 @@
+//pode colocar a entesão tanto jsx ou js
+
+import React from 'react'
+
+// export default function(){
+//     return <h1>Primeiro</h1>
+// }
+// ou 
+// export default () => <h1>Primeiro</h1>
+
+
+export default function(){
+    return "Teste"
+}
---
+++ react/exercicios/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react' //react tem que ser importado na hora de fazer as conversões
+import ReactDOM from 'react-dom'
+
+import Primeiro from './componentes/Primeiro'
+
+ReactDOM.render(<Primeiro/>, document.getElementById('root')) //jsx = js extendido, suporta passar html em função
+//para de separar em pastas de tecnologia e começa a ficar mais integrado e o framework faz os tranpile       
