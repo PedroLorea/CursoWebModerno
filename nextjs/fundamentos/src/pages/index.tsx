7669a4e4058cce04d0886c9077dea9fd653dff8a
PedroLorea <p.lorea@edu.pucrs.br>
2023-04-11 16:01:18 -0300

    nextjs

---
+++ nextjs/fundamentos/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { createElement } from "react"
+
+export default function Inicio(){
+    return (
+        <div>
+            <h1>Fundamentos de Next.js e React</h1>
+            <h2>Vamos estudar esse framework.</h2>
+        </div>
+    )
+}
---
+++ nextjs/fundamentos/src/pages/tsx.tsx
@@ -0,0 +1,23 @@
+export default function Tsx(){
+    const titulo = <h1>TSX é um conceito central!!!</h1>
+    const a = 2
+    const b = 3
+
+    function subtitulo(){
+        return <h2>{"muito muito legal legal".toUpperCase()}</h2>
+    }
+
+    return (
+        <div>
+            <h1>TSX é um conceito Central</h1>
+            {a * b } {/* {código javascript} */}
+            {titulo}
+            {subtitulo()}
+            <h2>{"muito legal".toUpperCase()}</h2>
+            <p>
+                {JSON.stringify({nome: 'João', idade: 30 })}
+            </p>
+        </div>
+    
+    )
+}
