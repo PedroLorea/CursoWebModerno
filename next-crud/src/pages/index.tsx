b64605df508efee5b3226235f54c0b1aa73b39f0
PedroLorea <p.lorea@edu.pucrs.br>
2023-05-09 14:59:00 -0300

    Construtor cliente, tabela clientes e funções

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,6 +1,24 @@
 import Layout from "../components/Layout"
+import Cliente from "../core/Cliente"
+import Tabela from "../components/Tabela"
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 90, '2'),
+    new Cliente('Pedro', 13, '3'),
+    new Cliente('Carlos', 64, '4'),
+  ]
+
+  function clienteSelecionado(cliente: Cliente){
+
+  } 
+  
+  function clienteExcluido(cliente: Cliente){
+
+  }
+
   return (
       <div className={`
         flex h-screen justify-center items-center
@@ -8,7 +26,9 @@ export default function Home() {
         text-white
         `}>
         <Layout titulo="Cadastro Simples">
-          <span>Conteudo</span>
+          <Tabela clientes={clientes}
+            clienteSelecionado={clienteSelecionado}
+            clienteExcluido={clienteExcluido}></Tabela>
         </Layout>
       </div>
   )
