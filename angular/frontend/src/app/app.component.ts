6165469f3095f851f8ba22234ab1ec6b0868c223
PedroLorea <p.lorea@edu.pucrs.br>
2023-05-17 16:06:23 -0300

    Angular componentes

---
+++ angular/frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  title = 'frontend';
+}
+
+// se colocar no html {{ title }} ele pega essa variavel
